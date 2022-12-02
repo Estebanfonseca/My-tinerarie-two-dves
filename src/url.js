e6647253cbbaeb75dea8aca7181b0b751e3ef987
edwin esteban peña fonseca <106789660+Estebanfonseca@users.noreply.github.com>
2022-12-02 08:14:08 -0500

    Update url.js

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
-let apiUrl = 'http://localhost:8080/api'
+let apiUrl = 'https://back-twodevs.up.railway.app/api/'
 
 if(process.env.NODE_ENV === 'production'){
     apiUrl = process.env.REACT_APP_URL
